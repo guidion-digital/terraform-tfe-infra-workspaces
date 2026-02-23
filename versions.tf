aef877fb14fbafcaa1fe59b8155da5161bb5a498
Eagleman7 <eagleman7@quicknet.nl>
2026-02-23 17:05:37 +0100

    feat: Allow newer versions BREAKING CHANGE: Versions have been upgraded

--- versions.tf
+++ versions.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 1.6.1, < 1.7.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.92.0"
+      version = "=> 5.0"
     }
 
     github = {
